0e4b748aa9d440a2209e70f686e389c63e65e6bd
Ananchai Phahupongsub <ananp603@chare.postbox.in.th>
2020-05-07 11:54:58 +0700

    refactor helper api

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
 export default axios.create({
   baseURL: `http://52.163.210.101:44000/`,
-  headers: {'x-access-token': sessionStorage.getItem('token')}
+  // baseURL: `http://localhost:44000/`,
+  // headers: {'x-access-token': localStorage.getItem('token')}
 });
